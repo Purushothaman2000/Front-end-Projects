a30cd6476ff97c6a65774578d00946706fbf6f40
Purushothaman2000 <purushothamanarumugam2000@gmail.com>
2024-09-17 14:14:32 +0530

    edit some codes

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,25 +1,23 @@
 let display = document.querySelector("#display")
 
 function AddtoDisplay(input){
-    display.value +=input
-   
+    display.value +=input  
 }
 
 function clearScreen(){
     display.value = ''
-
 }
+
 function calc(){
     try{
         display.value = eval(display.value)
     }
     catch(error){
         display.value='Error'
-
     } 
-
 }
-function re(){
+
+function backSpace(){
     display.value=display.value.toString().slice(0,-1)
 }
     
